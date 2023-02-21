313c9120fa578f582544fd3dc5e4eccf43dabdac
Ghayur Aarif Shah <ghayuraarifshah@gmail.com>
2023-02-21 21:10:23 +0530

    some work done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::fs;
+mod generate_code;
 mod parser;
-
 fn main() {
-    println!("Hello, world!");
+    let result = fs::read_to_string("input.json").unwrap();
+    let res = parser::parse(result).unwrap();
+    generate_code::generate(res[1].clone(), res[0].clone()).unwrap();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,11 @@
-use serde_json;
+use serde_json::{self, Value};
+use std::error::Error;
+pub fn parse(data: String) -> Result<Vec<Value>, Box<dyn Error>> {
+    let v: Value = serde_json::from_str(&data)?;
+    let types = v.get("types").ok_or("No types provided")?.to_owned();
+    let endpints = v
+        .get("endpoints")
+        .ok_or("No endpoints provided")?
+        .to_owned();
+    Ok(vec![endpints, types])
+}
