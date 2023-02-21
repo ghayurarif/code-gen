2f7805a067d46130a10535e688a77e7e51be763c
Ghayur Aarif Shah <ghayuraarifshah@gmail.com>
2023-02-21 22:42:18 +0530

    types implemented

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod parser;
 fn main() {
     let result = fs::read_to_string("input.json").unwrap();
     let res = parser::parse(result).unwrap();
-    generate_code::generate(res[1].clone(), res[0].clone()).unwrap();
+    let (_types, _endpoints) = generate_code::generate(res[1].clone(), res[0].clone()).unwrap();
 }
